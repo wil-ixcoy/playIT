0e6137c9aac0be6f8cd02e8e9ead474316bc4605
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2023-01-28 16:01:33 -0600

    creacion del esquema para categoria

---
+++ schemas/category.schema.js
@@ -0,0 +1,29 @@
+const joi = require("joi");
+const { password } = require("pg/lib/defaults");
+
+let id = joi.number().integer();
+let name = joi.string().min(3).max(30);
+let description = joi.string().min(25).max(140);
+let cover = joi.string();
+
+const createCategorySchema = joi.object({
+  name: name.required(),
+  description: description.required(),
+  cover: cover.required(),
+});
+
+const getCategorySchema = joi.object({
+  id: id.required(),
+});
+
+const updateCategorySchema = joi.object({
+  name: name,
+  description: description,
+  cover: cover,
+});
+
+module.exports = {
+  createCategorySchema,
+  getCategorySchema,
+  updateCategorySchema,
+};
